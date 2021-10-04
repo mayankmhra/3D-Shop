d65e1336b6b862fcb5a95049dacba2cc1c21295f
mayankmhra <mayank.336677@gmail.com>
2021-10-05 01:56:03 +0530

    Updates

---
+++ precache-manifest.cb97fabff499d9ebb06b439ff10f3369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b766da853559f1f577379d4580383c8b",
+    "url": "/3D-Shop/index.html"
+  },
+  {
+    "revision": "40a9b4e2634627c2396c",
+    "url": "/3D-Shop/static/css/main.bfeb3117.chunk.css"
+  },
+  {
+    "revision": "6f5ec18c4c1ad45ab302",
+    "url": "/3D-Shop/static/js/2.9e04b79e.chunk.js"
+  },
+  {
+    "revision": "bd16ffa8a645b42adcb7fc65d4c790f8",
+    "url": "/3D-Shop/static/js/2.9e04b79e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40a9b4e2634627c2396c",
+    "url": "/3D-Shop/static/js/main.db22b48f.chunk.js"
+  },
+  {
+    "revision": "5df272904275d033f444",
+    "url": "/3D-Shop/static/js/runtime-main.de3d810e.js"
+  }
+]);
